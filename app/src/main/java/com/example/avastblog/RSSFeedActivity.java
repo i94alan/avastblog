e5dac3239d11b7c0184a8b5dd06ad825eb666e6a
Alan Chang <achang@sierrawireless.com>
2020-08-27 16:42:22 +0800

    initial version of avastblog

---
+++ app/src/main/java/com/example/avastblog/RSSFeedActivity.java
@@ -0,0 +1,139 @@
+package com.example.avastblog;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RSSFeedActivity extends ListActivity {
+
+    private ProgressBar pDialog;
+    ArrayList<HashMap<String, String>> rssItemList = new ArrayList<>();
+    RSSParser rssParser = new RSSParser();
+    List<RSSItem> rssItems = new ArrayList<>();
+    private static String TAG_TITLE = "title";
+    private static String TAG_LINK = "link";
+    private static String TAG_PUB_DATE = "pubDate";
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+        if(!isNetworkConnected()) {
+            Toast.makeText(this, "Network unavailable. Try again.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+//        String rss_link = getIntent().getStringExtra("rssLink");
+        String rss_link = "https://blog.avast.com/rss.xml";
+        new LoadRSSFeedItems().execute(rss_link);
+        ListView lv = getListView();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent in = new Intent(getApplicationContext(), WebActivity.class);
+                String page_url = ((TextView) view.findViewById(R.id.page_url)).getText().toString().trim();
+                in.putExtra("url", page_url);
+                startActivity(in);
+            }
+        });
+    }
+
+    public class LoadRSSFeedItems extends AsyncTask<String, String, String> {
+        private final static int RSS_COUNT = 5;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressBar(RSSFeedActivity.this, null, android.R.attr.progressBarStyleLarge);
+            RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+            pDialog.setLayoutParams(layoutParams);
+            pDialog.setVisibility(View.VISIBLE);
+            relativeLayout.addView(pDialog);
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // rss link url
+            String rss_url = args[0];
+            // list of rss items
+            rssItems = rssParser.getRSSFeedItems(rss_url);
+            //limit to latest 5 blogs
+            rssItems = rssItems.subList(0,RSS_COUNT);
+            // looping through each item
+            for (final RSSItem item : rssItems) {
+                // creating new HashMap
+                if (item.link.toString().equals(""))
+                    break;
+                HashMap<String, String> map = new HashMap<String, String>();
+
+//                // adding each child node to HashMap key => value
+//                String givenDateString = item.pubdate.trim();
+//                SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+//                try {
+//                    Date mDate = sdf.parse(givenDateString);
+//                    SimpleDateFormat sdf2 = new SimpleDateFormat("EEEE, dd MMMM yyyy - hh:mm a", Locale.US);
+//                    item.pubdate = sdf2.format(mDate);
+//
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//
+//                }
+
+                map.put(TAG_TITLE, item.title);
+                map.put(TAG_LINK, item.link);
+//                map.put(TAG_PUB_DATE, item.pubdate);
+                // adding HashList to ArrayList
+                rssItemList.add(map);
+            }
+
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    ListAdapter adapter = new SimpleAdapter(
+                            RSSFeedActivity.this,
+                            rssItemList, R.layout.rss_item_list_row,
+                            new String[]{TAG_LINK, TAG_TITLE,},
+                            new int[]{R.id.page_url, R.id.title});
+
+                    // updating listview
+                    setListAdapter(adapter);
+                }
+            });
+            return null;
+        }
+
+        protected void onPostExecute(String args) {
+            pDialog.setVisibility(View.GONE);
+        }
+    }
+}
